f39c551c71578695b3d9e014110e621960e41555
Chris Soueidan <chris.soueidan@gmail.com>
2017-05-11 18:19:01 -0400

    added youtube video fct
    stop on modal close

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,13 @@ $(document).ready(function () {
         $('.respond').toggleClass('hidden-xs visible-xs');
     });
 
+    // Stop Youtube video on modal close -- Finds all iframes from youtubes and gives them a unique class
+    jQuery('iframe[src*="https://www.youtube.com/embed/"]').addClass("youtube-iframe");
+    jQuery(".stop").click(function () {
+        // changes the iframe src to prevent playback or stop the video playback in our case
+        $('.youtube-iframe').each(function (index) {
+            $(this).attr('src', $(this).attr('src'));
+            return false;
+        });
+    });
 });
